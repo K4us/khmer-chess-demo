e480b5a12a98e29f12eae7087ed95e4e5bc72ecb
raksa <eng.raksa@gmail.com>
2022-10-15 22:47:06 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 
-import KhmerChessBoardComp from "./KhmerChessBoardComp";
+import { KhmerChessBoardComp, PIECE_COLOR_BLACK } from "khmer-chess-board";
 import kcb from "khmer-chess-board/package.json";
 import kcai from "khmer-chess-ai/package.json";
+import { KhmerChessAI } from "khmer-chess-ai";
 
 export default function App() {
     return (
@@ -10,10 +11,28 @@ export default function App() {
             <h3>Khmer Chess by K4us</h3>
             <KhmerChessBoardComp
                 width={600}
-                ref={(kcbc) => {
-                    (window as any).kcbc = kcbc;
-                }}
-            />
+                isQuickMove={true}
+                isSoundEnabled={true}
+                load={(kcb) => {
+                    (window as any).kcb = kcb;
+                    const khmerChessAI = new KhmerChessAI({
+                        khmerChess: kcb.khmerChess,
+                        turn: PIECE_COLOR_BLACK,
+                    });
+                    kcb.boardManager.addBoardStatusEventListener((boardStatusEvent) => {
+                        if (boardStatusEvent.isWin) {
+                            alert(boardStatusEvent.message);
+                        } else if (boardStatusEvent.isMoving && khmerChessAI.isAITurn) {
+                            const result = khmerChessAI.attemptMove();
+                            if (result) {
+                                kcb.move(result.fromIndex, result.toIndex);
+                            } else {
+                                alert("Fail to attempt move");
+                            }
+                        }
+                    });
+                    kcb.playManager.play();
+                }} />
             <h4>Build With:</h4>
             <a href={kcb.homepage}>Khmer Chess Board v{kcb.version}</a>
             <br />
